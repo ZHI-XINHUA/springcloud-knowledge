0e24bc46dc79f72aad68ef0af4251f21ba7a1536
1960881192@qq.com <1960881192@qq.com>
2019-03-18 17:03:02 +0800

    eureka-server

---
+++ SpringCloud-Leaning-GamdenSR4/consumer-movie-ribbon-hystrix/src/main/java/cloud/ConsumerMovieApp.java
@@ -0,0 +1,31 @@
+package cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Hello world!
+ *
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableCircuitBreaker // 或 @EnableHystrix
+public class ConsumerMovieApp
+{
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ConsumerMovieApp.class,args);
+    }
+}
