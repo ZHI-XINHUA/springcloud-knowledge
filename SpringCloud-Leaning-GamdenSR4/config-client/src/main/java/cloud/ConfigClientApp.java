57f264e15ca03ef19a78718967d5a695e5caac16
1960881192@qq.com <1960881192@qq.com>
2019-10-12 16:29:24 +0800

    spring cloud config

---
+++ SpringCloud-Leaning-GamdenSR4/config-client/src/main/java/cloud/ConfigClientApp.java
@@ -0,0 +1,18 @@
+package cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ConfigClientApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ConfigClientApp.class,args);
+    }
+}
