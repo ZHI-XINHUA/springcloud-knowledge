cdebe91bd9ce77d502b018720c288faccc92d40e
1960881192@qq.com <1960881192@qq.com>
2019-10-17 17:24:22 +0800

    spring cloud config 手动刷新

---
+++ SpringCloud-Leaning-GamdenSR4/config-client-retry/src/main/java/cloud/ConfigClientRetryApp.java
@@ -0,0 +1,18 @@
+package cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+public class ConfigClientRetryApp
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(ConfigClientRetryApp.class,args);
+    }
+}
