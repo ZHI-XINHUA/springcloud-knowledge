66090e0f8884514dcf4c332f437783af6d587954
zhixinhua <793597462@qq.com>
2020-03-29 21:01:23 +0800

    eureka 集群

---
+++ cloud2020/cloud-eureka-server7002/src/main/java/com/zxh/springcloud/EurekaMain.java
@@ -0,0 +1,16 @@
+package com.zxh.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer //开启Eureka Server
+public class EurekaMain
+{
+    public static void main( String[] args )
+    {
+
+        SpringApplication.run(EurekaMain.class,args);
+    }
+}
